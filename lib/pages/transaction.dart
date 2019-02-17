366897b534c40a296fb6c0499e44d761d88ca5c0
3Cement <danielmilewski123@gmail.com>
2019-02-17 17:15:05 +0100

    Back button value added

--- lib/pages/transaction.dart
+++ lib/pages/transaction.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+
 class TransactionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Transaction details'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('About transaction'),
-          RaisedButton(
-            color: Theme.of(context).accentColor,
-            child: Text('DELETE'),
-            onPressed: () => Navigator.pop(context, true),
-          )
-        ],
+    return WillPopScope(
+      onWillPop: () {
+        print('Back button pressed!');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Transaction details'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('About transaction'),
+            RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('DELETE'),
+              onPressed: () => Navigator.pop(context, true),
+            )
+          ],
+        ),
       ),
     );
   }
