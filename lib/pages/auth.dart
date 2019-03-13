8d30e96e4a38565c48173be8682a37c5bca9fdce
3Cement <danielmilewski123@gmail.com>
2019-03-13 20:25:12 +0100

    Switch button added to accept terms

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -10,6 +10,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String _emailValue;
   String _passwordValue;
+  bool _acceptTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +39,15 @@ class _AuthPageState extends State<AuthPage> {
                     _passwordValue = value;
                   });
                 },
+              ),
+              SwitchListTile(
+                value: _acceptTerms,
+                onChanged: (bool value) {
+                  setState(() {
+                    _acceptTerms = value;
+                  });
+                },
+                title: Text('Accept Terms'),
               ),
               SizedBox(
                 height: 10.0,
